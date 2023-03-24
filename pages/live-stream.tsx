5b353d92305c6d1927bf5b18f291186df93d58b3
bsartain <brett.sartain@comporium.com>
2023-03-24 16:40:19 -0400

    New gitignore

--- pages/live-stream.tsx
+++ pages/live-stream.tsx
@@ -177,7 +177,7 @@ const OrderOfService = ({ data, orderOfService }) => {
                     width='100%'
                     height='500'
                     src={`${youtubeLink}`}
-                    title='Hill City YouTube Live Stream'
+                    title='YouTube Live Stream: Hill City Church'
                     frameBorder='0'
                     allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                     allowFullScreen
