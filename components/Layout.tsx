6f2d8aed19f65925f0374f46371db85e6b9c1ff8
bsartain <brett.sartain@comporium.com>
2023-03-24 20:30:17 -0400

    Added Favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -68,8 +68,9 @@ function Layout({ children, title, meta }: Props) {
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        {/* <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' /> */}
+        <link rel='icon' type='image/png' sizes='32x32' href='/images/favicon-32x32.png'></link>
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5'></link>
         <meta charSet='utf-8' />
         <meta
