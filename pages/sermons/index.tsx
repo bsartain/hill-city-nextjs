d459cf2fccc10a7256daf80f37b376858c684aa6
bsartain <brett.sartain@comporium.com>
2023-04-04 15:51:23 -0400

    Added sermon filter

--- pages/sermons/index.tsx
+++ pages/sermons/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Store } from "context/context";
 import Layout from "components/Layout";
 import Header from "components/Header";
@@ -6,14 +6,49 @@ import { useRouter } from "next/router";
 import { formatDate } from "utils";
 import ReactPaginate from "react-paginate";
 import HeadingDivider from "components/HeadingDivider";
+import { Spinner } from "react-bootstrap";
+
+let params = { preacher: "", sermonSeries: "", book: "" };
 
 const SermonsPage = ({ data }) => {
   const router = useRouter();
   const { singleSermon, setSingleSermon } = useContext(Store);
+  const [sermonFilteredParameters, setSermonFilteredParameters] = useState({
+    preacher: "",
+    sermonSeries: "",
+    book: "",
+  });
+  const [spinner, setSpinner] = useState(false);
   const [sermons, setSermons] = useState(data.slice(0, 300));
   const [pageNumber, setPageNumber] = useState(0);
   const sermonsPerPage = 12;
   const pagesVisited = pageNumber * sermonsPerPage;
+
+  const preacherList: any = [];
+  const seriesList = [];
+  const bibleBookList = [];
+
+  data.forEach((item: any) => {
+    if (item.preacher) {
+      item.preacher.forEach((preacher: any) => {
+        preacherList.push(preacher.name);
+      });
+    }
+    if (item.sermon_series) {
+      item.sermon_series.forEach((series: any) => {
+        seriesList.push(series.name);
+      });
+    }
+    if (item.bible_book) {
+      item.bible_book.forEach((book: any) => {
+        bibleBookList.push(book.name);
+      });
+    }
+  });
+
+  const preachers = Array.from(new Set(preacherList));
+  const series = Array.from(new Set(seriesList));
+  const bibleBook = Array.from(new Set(bibleBookList));
 
   const displaySermons = sermons
     .slice(pagesVisited, pagesVisited + sermonsPerPage)
@@ -82,6 +117,42 @@ const SermonsPage = ({ data }) => {
     },
   };
 
+  const getFilteredSermons = async (value: string, param: string) => {
+    setSpinner(true);
+    if (param === "preacher") {
+      params.preacher = value;
+    } else if (param === "sermonSeries") {
+      params.sermonSeries = value;
+    } else if (param === "book") {
+      params.book = value;
+    }
+
+    const myUrlWithParams = new URL("https://hillcitysc.com/wp-json/hc/v1/hc-sermons");
+
+    if (params.preacher) {
+      myUrlWithParams.searchParams.append("preacher", params.preacher);
+    }
+    if (params.sermonSeries) {
+      myUrlWithParams.searchParams.append("series", params.sermonSeries);
+    }
+    if (params.book) {
+      myUrlWithParams.searchParams.append("book", params.book);
+    }
+
+    const response: any = await fetch(myUrlWithParams.href, {
+      method: "GET",
+      headers: {
+        Accept: "application/json;version=1",
+      },
+    });
+
+    const json = await response.json();
+    setSpinner(false);
+    setSermons(json.slice(0, 300));
+  };
+
+  useEffect(() => {}, [sermonFilteredParameters, setSermonFilteredParameters]);
+
   return (
     <Layout title='Hill City Church: Sermons' meta={meta}>
       <span>
@@ -113,6 +184,89 @@ const SermonsPage = ({ data }) => {
           </div>
           <h1>Current Sermons</h1>
           <HeadingDivider />
+          <div className='d-flex justify-content-center mt-4 mb-4 sermon-filter'>
+            {spinner ? (
+              <Spinner animation='border' style={{ marginTop: "25px", marginRight: "10px" }} />
+            ) : null}
+            <div className='mb-4 me-2'>
+              <label>Preacher</label>
+              <select
+                className='form-select'
+                value={sermonFilteredParameters[0]}
+                onChange={(e) => {
+                  {
+                    setSermonFilteredParameters({
+                      ...sermonFilteredParameters,
+                      preacher: e.target.value,
+                    });
+                    getFilteredSermons(e.target.value, "preacher");
+                  }
+                }}>
+                <option value=''>...</option>
+                {preachers
+                  ? preachers.map((preacher: any, index: number) => {
+                      return (
+                        <option value={preacher} key={index}>
+                          {preacher}
+                        </option>
+                      );
+                    })
+                  : null}
+              </select>
+            </div>
+            <div className='mb-4 me-2'>
+              <label>Series</label>
+              <select
+                className='form-select'
+                value={sermonFilteredParameters[1]}
+                onChange={(e) => {
+                  {
+                    setSermonFilteredParameters({
+                      ...sermonFilteredParameters,
+                      sermonSeries: e.target.value,
+                    });
+                    getFilteredSermons(e.target.value, "sermonSeries");
+                  }
+                }}>
+                <option value=''>...</option>
+                {series
+                  ? series.map((series: any, index: number) => {
+                      return (
+                        <option value={series} key={index}>
+                          {series}
+                        </option>
+                      );
+                    })
+                  : null}
+              </select>
+            </div>
+            <div className='mb-4'>
+              <label>Bible Book</label>
+              <select
+                className='form-select'
+                value={sermonFilteredParameters[2]}
+                onChange={(e) => {
+                  {
+                    setSermonFilteredParameters({
+                      ...sermonFilteredParameters,
+                      book: e.target.value,
+                    });
+                    getFilteredSermons(e.target.value, "book");
+                  }
+                }}>
+                <option value=''>...</option>
+                {bibleBook
+                  ? bibleBook.map((book: any, index: number) => {
+                      return (
+                        <option value={book} key={index}>
+                          {book}
+                        </option>
+                      );
+                    })
+                  : null}
+              </select>
+            </div>
+          </div>
           <div className='row d-flex justify-content-center'>
             {displaySermons}
             <div className='pagination-container'>
